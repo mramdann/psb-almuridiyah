e1230d77c583dd37eb48e9bfc06574c189a76eaf
anam321 <jackston9@gmail.com>
2021-08-05 20:30:32 +0700

    add template piblik dan koneksi database, dan menambah form pendaftaran halam publik

---
+++ form_registrasi.php
@@ -0,0 +1,356 @@
+<?php include "header.php";
+include "koneksi.php";
+
+
+
+if (isset($_POST['daftar'])) {
+
+    // INPUT DATA DIRI PESERTA
+
+    $nama = $_POST['nama'];
+    $kelamin = $_POST['kelamin'];
+    $nik = $_POST['nik'];
+    $kk = $_POST['kk'];
+    $tempat_lahir = $_POST['tempat_lahir'];
+    $tgl_lahir = $_POST['tgl_lahir'];
+    $hp = $_POST['hp'];
+    $agama = $_POST['agama'];
+    $alamat = $_POST['alamat'];
+    $id_ayah = $_POST['id_ayah'];
+    $id_ibu = $_POST['id_ibu'];
+
+
+    // INPUT DATA JALUR PENDIDIKAN
+
+    $jenis_pendaftaran = $_POST['jenis_pendaftaran'];
+    $jenjang = $_POST['jenjang'];
+    $asal_sekolah = $_POST['asal_sekolah'];
+    $jalur_pendaftaran = $_POST['jalur_pendaftaran'];
+
+    // INPUT DATA AYAH
+
+    $nama_ayah = $_POST['nama_ayah'];
+    $thn_lahir = $_POST['thn_lahir'];
+    $pendidikan = $_POST['pendidikan'];
+    $pekerjaan = $_POST['pekerjaan'];
+
+
+    // INPUT DATA IBU
+
+    $nama_ibu = $_POST['nama_ibu'];
+    $thn_lahir_ibu = $_POST['thn_lahir_ibu'];
+    $pendidikan_ibu = $_POST['pendidikan_ibu'];
+    $pekerjaan_ibu = $_POST['pekerjaan_ibu'];
+
+
+
+    // Ambil Data Gambar yang Dikirim dari Form
+
+    $query = $koneksi->query("INSERT INTO tbl_peserta (nama, jenis_kelamin, nik, no_kk, tempat_lahir, tgl_lahir, no_hp, agama, alamat, foto, jenis_pendaftaran, jenjang, asal_sekolah, jalur_pendaftaran, id_ayah, id_ibu) VALUES ('$nama','$kelamin','$nik','$kk','$tempat_lahir','$tgl_lahir','$hp','$agama','$alamat','$foto','$jenis_pendaftaran','$jenjang','$asal_sekolah','$jalur_pendaftaran','$id_ayah','$id_ibu')");
+
+    // $query2 = $koneksi->query("INSERT INTO tbl_ayah_kandung (nama_ayah, thn_lahir, pendidikan, pekerjaan, waktu_keluar, status_ijin) VALUES ('$nam_ayah','$thn_lahir','$pendidikan','$pekerjaan')");
+
+    // $query3 = $koneksi->query("INSERT INTO tbl_ibu_kandung (nama_ibu, thn_lahir, pendidikan, pekerjaan, waktu_keluar, status_ijin) VALUES ('$nam_ibu','$thn_lahir_ibu','$pendidikan_ibu','$pekerjaan_ibu')");
+
+
+    echo "<script>alert('data berhasil di tambahkan ! ...')</script>";
+    echo "<script>location='form_registrasi.php'</script>";
+}
+
+
+
+?>
+
+<div class="site-section ftco-subscribe-1 site-blocks-cover pb-4" style="background-image:url(images/xbg_1.jpg.pagespeed.ic.bYKh1zv1gk.jpg)">
+    <div class="container">
+        <div class="row align-items-end">
+            <div class="col-lg-7">
+                <h2 class="mb-0">Registrasi</h2>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="site-section">
+    <div class="container">
+        <div class="row mb-5 justify-content-center text-center">
+            <div class="col-lg-6 mb-5">
+                <h2 class="section-title-underline mb-3">
+                    <span>Form Pendaftaran</span>
+                </h2>
+            </div>
+        </div>
+
+        <form action="" method="POST">
+
+            <div class="card">
+                <div class="card-body">
+
+                    <!--================= DATA PRIBADI ================================-->
+                    <!--================= DATA PRIBADI ================================-->
+
+                    <div class="row mb-5">
+                        <div class="col-lg-3">
+                            <h4 class="section-title-underline mb-3"><span>Data Diri</span></h4>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6 form-group">
+                            <label for="nama">Nama</label>
+                            <input type="text" name="nama" id="nama" class="form-control form-control-lg" placeholder="Nama Harus Sesuai Dengan KTP Atatu Ijazah">
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="kelamin">Jenis Kelamin</label>
+                            <select class="form-control" name="kelamin" id="kelamin">
+                                <option>PILIH :</option>
+                                <option value="Laki-Laki">Laki-Laki</option>
+                                <option value="Perempuan">Perempuan</option>
+                            </select>
+                        </div>
+
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6 form-group">
+                            <label for="nik">NIK</label>
+                            <input type="number" name="nik" id="nik" class="form-control form-control-lg">
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="kk">Nomor KK</label>
+                            <input type="number" name="kk" id="kk" class="form-control form-control-lg">
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="tempat_lahir">Tempat Lahir</label>
+                            <input type="text" name="tempat_lahir" id="tempat_lahir" class="form-control form-control-lg">
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="tgl_lahir">Tanggal Lahir</label>
+                            <input type="date" name="tgl_lahir" id="tgl_lahir" class="form-control form-control-lg">
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="hp">No HP</label>
+                            <input type="number" name="hp" id="hp" class="form-control form-control-lg">
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="foto">Foto</label>
+                            <input type="file" name="foto" id="foto" class="form-control form-control-lg">
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="agama">Agama</label>
+                            <select class="form-control" name="agama" id="agama">
+                                <option>PILIH :</option>
+                                <option value="Islam">Islam</option>
+                                <option value="Kristen">Kristen</option>
+                                <option value="Budha">Budha</option>
+                                <option value="Konghuchu">Konghuchu</option>
+                            </select>
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="alamat">Alamat</label>
+                            <textarea name="alamat" id="alamat" cols="30" rows="10" class="form-control"></textarea>
+                        </div>
+                    </div>
+
+
+                    <br>
+
+                    <!--================= END DATA PRIBADI ================================-->
+                    <!--================= END DATA PRIBADI ================================-->
+
+
+                    <!--================= END DATA PEDIDIKAN ================================-->
+                    <!--================= END DATA PEDIDIKAN ================================-->
+
+                    <div class="row mb-5">
+                        <div class="col-lg-3">
+                            <h4 class="section-title-underline mb-3"><span>Pendidikan</span></h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 form-group">
+                            <label for="jenis_pendaftaran">Jenis Pendaftaran</label>
+                            <select class="form-control" name="jenis_pendaftaran" id="jenis_pendaftaran">
+                                <option>PILIH :</option>
+                                <?php
+                                $sql = $koneksi->query("SELECT * FROM tbl_regist");
+                                while ($data = $sql->fetch_assoc()) {
+                                    # code...
+                                ?>
+                                    <option value="<?= $data['jenis_pendaftaran'] ?>"><?= $data['jenis_pendaftaran'] ?></option>
+                                <?php
+                                } ?>
+                            </select>
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="jenjang">Pilihan Jenjang</label>
+                            <select class="form-control" name="jenjang" id="jenjang">
+                                <option>PILIH :</option>
+                                <?php
+                                $sql = $koneksi->query("SELECT * FROM tbl_regist");
+                                while ($data = $sql->fetch_assoc()) {
+                                    # code...
+                                ?>
+                                    <option value="<?= $data['jenjang'] ?>"><?= $data['jenjang'] ?></option>
+                                <?php
+                                } ?>
+                            </select>
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="asal_sekolah">Asal Sekolah</label>
+                            <input type="text" name="asal_sekolah" id="asal_sekolah" class="form-control form-control-lg" placeholder="Masukan Asal Sekolah Anda">
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="jalur_pendaftaran">Jalur Pendaftaran</label>
+                            <select class="form-control" name="jalur_pendaftaran" id="jalur_pendaftaran">
+                                <option>PILIH :</option>
+                                <?php
+                                $sql = $koneksi->query("SELECT * FROM tbl_regist");
+                                while ($data = $sql->fetch_assoc()) {
+                                    # code...
+                                ?>
+                                    <option value="<?= $data['jalur_pendaftaran'] ?>"><?= $data['jalur_pendaftaran'] ?></option>
+                                <?php
+                                } ?>
+                            </select>
+                        </div>
+                    </div>
+
+                    <!--================= END DATA PEDIDIKAN ================================-->
+                    <!--================= END DATA PEDIDIKAN ================================-->
+
+                    <!--================= DATA AYAH KANDUNG ================================-->
+                    <!--================= DATA AYAH KANDUNG ================================-->
+                    <div class="row mb-5">
+                        <div class="col-lg-3">
+                            <h4 class="section-title-underline mb-3"><span>Data Ayah Kandung</span></h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 form-group">
+                            <label for="nama_ayah">Nama Ayah Kandung</label>
+                            <input type="text" name="nama_ayah" id="nama_ayah" class="form-control form-control-lg" placeholder="Nama Asli Ayah Kandung. Contoh : 1995">
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="thn_lahir">Tahun Lahir</label>
+                            <input type="text" name="thn_lahir" id="thn_lahir" class="form-control form-control-lg" placeholder="Tahun Lahir Ayah Kandung. Contoh : 1995">
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="pendidikan">Pendidikan</label>
+                            <select class="form-control" name="pendidikan" id="pendidikan">
+                                <option>PILIH :</option>
+                                <?php
+                                $sql = $koneksi->query("SELECT * FROM tbl_pendidikan");
+                                while ($data = $sql->fetch_assoc()) {
+                                    # code...
+                                ?>
+                                    <option value="<?= $data['pendidikan'] ?>"><?= $data['pendidikan'] ?></option>
+                                <?php
+                                } ?>
+                            </select>
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="pekerjaan">Pekerjaan</label>
+                            <select class="form-control" name="pekerjaan" id="pekerjaan">
+                                <option>PILIH :</option>
+                                <?php
+                                $sql = $koneksi->query("SELECT * FROM tbl_pekerjaan");
+                                while ($data = $sql->fetch_assoc()) {
+                                    # code...
+                                ?>
+                                    <option value="<?= $data['pekerjaan'] ?>"><?= $data['pekerjaan'] ?></option>
+                                <?php
+                                } ?>
+                            </select>
+                        </div>
+                    </div>
+
+                    <!--================= END DATA AYAH KANDUNG ================================-->
+                    <!--================= END DATA AYAH KANDUNG ================================-->
+
+                    <!--================= DATA Ibu KANDUNG ================================-->
+                    <!--================= DATA Ibu KANDUNG ================================-->
+
+                    <div class="row mb-5">
+                        <div class="col-lg-3">
+                            <h4 class="section-title-underline mb-3"><span>Data Ibu Kandung</span></h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 form-group">
+                            <label for="nama_ibu">Nama Ibu Kandung</label>
+                            <input type="text" name="nama_ibu" id="nama_ibu" class="form-control form-control-lg" placeholder="Nama Asli Ibu Kandung">
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="thn_lahir_ibu">Tahun Lahir</label>
+                            <input type="text" name="thn_lahir_ibu" id="thn_lahir_ibu" class="form-control form-control-lg" placeholder="Tahun Lahir Ibu Kandung. Contoh : 1995">
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="pendidikan_ibu">Pendidikan</label>
+                            <select class="form-control" name="pendidikan_ibu" id="pendidikan_ibu">
+                                <option>PILIH :</option>
+                                <?php
+                                $sql = $koneksi->query("SELECT * FROM tbl_pendidikan");
+                                while ($data = $sql->fetch_assoc()) {
+                                    # code...
+                                ?>
+                                    <option value="<?= $data['pendidikan'] ?>"><?= $data['pendidikan'] ?></option>
+                                <?php
+                                } ?>
+                            </select>
+                        </div>
+
+                        <div class="col-md-6 form-group">
+                            <label for="pekerjaan_ibu">Pekerjaan</label>
+                            <select class="form-control" name="pekerjaan_ibu" id="pekerjaan_ibu">
+                                <option>PILIH :</option>
+                                <?php
+                                $sql = $koneksi->query("SELECT * FROM tbl_pekerjaan");
+                                while ($data = $sql->fetch_assoc()) {
+                                    # code...
+                                ?>
+                                    <option value="<?= $data['pekerjaan'] ?>"><?= $data['pekerjaan'] ?></option>
+                                <?php
+                                } ?>
+                            </select>
+                        </div>
+                    </div>
+
+                    <!--================= END DATA ibu KANDUNG ================================-->
+                    <!--================= END DATA ibu KANDUNG ================================-->
+
+                    <div class="row justify-content-center align-items-center">
+                        <div class="col-12">
+                            <button type="submit" name="daftar" class="btn btn-primary ">Daftar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </form>
+
+
+    </div>
+</div>
+
+
+
+<?php include "footer.php"; ?>
---
+++ header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from preview.colorlib.com/theme/academics/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 04 Aug 2021 15:05:45 GMT -->
+
+<head>
+    <title>Academics &mdash; Website by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="assets/front/fonts%2c_icomoon%2c_style.css%2bcss%2c_bootstrap.min.css%2bcss%2c_jquery-ui.css%2bcss%2c_owl.carousel.min.css%2bcss%2c_owl.theme.default.min.css%2bcss%2c_owl.theme.default.min.css%2bcss%2c_jquery.fancyb" />
+    <link href="assets/front/css/A.jquery.mb.YTPlayer.min.css.pagespeed.cf.N2G3N4O1x5.css" media="all" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="assets/front/css/A.style.css.pagespeed.cf.SghS18xLJ1.css">
+</head>
+
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+    <div class="site-wrap">
+        <div class="site-mobile-menu site-navbar-target">
+            <div class="site-mobile-menu-header">
+                <div class="site-mobile-menu-close mt-3">
+                    <span class="icon-close2 js-menu-toggle"></span>
+                </div>
+            </div>
+            <div class="site-mobile-menu-body"></div>
+        </div>
+        <div class="py-2 bg-light">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-9 d-none d-lg-block">
+                        <a href="#" class="small mr-3"><span class="icon-question-circle-o mr-2"></span> Have a questions?</a>
+                        <a href="#" class="small mr-3"><span class="icon-phone2 mr-2"></span> 10 20 123 456</a>
+                        <a href="#" class="small mr-3"><span class="icon-envelope-o mr-2"></span> <span class="__cf_email__" data-cfemail="a9c0c7cfc6e9c4d0cdc6c4c8c0c787cac6c4">[email&#160;protected]</span></a>
+                    </div>
+                    <div class="col-lg-3 text-right">
+                        <a href="http://localhost/psb-almuridiyah/admin/" class="small mr-3"><span class="icon-unlock-alt"></span> Log In</a>
+                        <a href="form_registrasi.php" class="small btn btn-primary px-4 py-2 rounded-0"><span class="icon-users"></span> Register</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+            <div class="container">
+                <div class="d-flex align-items-center">
+                    <div class="site-logo">
+                        <a href="index-2.html" class="d-block">
+                            <img src="assets/front/images/xlogo.jpg.pagespeed.ic.fRho2_GusO.jpg" alt="Image" class="img-fluid">
+                        </a>
+                    </div>
+                    <div class="mr-auto">
+                        <nav class="site-navigation position-relative text-right" role="navigation">
+                            <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                                <li class="active">
+                                    <a href="http://localhost/psb-almuridiyah" class="nav-link text-left">Home</a>
+                                </li>
+
+                                <li>
+                                    <a href="form_registrasi.php" class="nav-link text-left">About</a>
+                                </li>
+
+                                <li>
+                                    <a href="contact.php" class="nav-link text-left">Contact</a>
+                                </li>
+                            </ul>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="ml-auto">
+                        <div class="social-wrap">
+                            <a href="#"><span class="icon-facebook"></span></a>
+                            <a href="#"><span class="icon-twitter"></span></a>
+                            <a href="#"><span class="icon-linkedin"></span></a>
+                            <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
